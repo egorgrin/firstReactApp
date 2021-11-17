9daaf907c9edd3b3f87846daa2812ad6045ff980
egorgrin <egorgrin@gmail.com>
2021-11-17 13:25:27 +0300

    initial commit

---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+function Technologies() {
+  return (
+    <div>
+      <ul>
+        <li>scc</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Technologies;
