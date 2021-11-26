1dbda735132b9bff97b03207bf3b7e21cadfd9dc
egorgrin <egorgrin@gmail.com>
2021-11-26 16:17:53 +0300

    footer pushed down

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import './App.css';
-import '../node_modules/reset-css/reset.css';
-import '../node_modules/normalize.css/normalize.css';
+// import '../node_modules/reset-css/reset.css';
+// import '../node_modules/normalize.css/normalize.css';
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <header className="header">
-        {/* <img src="https://dummyimage.com/250x60/b0b0b0/fff.png" alt="" /> */}
-        <h1>LogoName</h1>
-      </header>
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://dummyimage.com/250x60/a3a3a3/fff.png&text=Logotype"
+                    alt=""
+                />
+            </header>
 
-      <aside className="sidebar">
-        <div>
-          <ul>
-            <li>
-              <a href="#">Profile</a>
-            </li>
-            <li>
-              <a href="#">Messages</a>
-            </li>
-            <li>
-              <a href="#">News</a>
-            </li>
-            <li>
-              <a href="#">Music</a>
-            </li>
-          </ul>
+            <div className="main">
+                <aside className="sidebar">
+                    <div className="sidebar__menu">
+                        <ul>
+                            <li>
+                                <a href="#">Profile</a>
+                            </li>
+                            <li>
+                                <a href="#">Messages</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                            <li>
+                                <a href="#">Music</a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+
+                <section className="content">
+                    <h1>Hello world!</h1>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Alias eum delectus ratione quaerat quis! Quasi
+                        quod, vero voluptas maxime quaerat itaque odio esse modi
+                        excepturi, officiis asperiores accusantium architecto ut
+                        qui sint necessitatibus est veniam magni numquam
+                        adipisci. Tempore, molestias!
+                    </p>
+                    <h3>Bye world!</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. In ullam dolores odio eum ea enim hic, explicabo
+                        itaque eos fuga aliquam nostrum debitis unde!
+                        Exercitationem labore sequi adipisci. Iste similique
+                        molestiae ab porro facilis mollitia aliquid, nisi
+                        consequuntur nam aspernatur?
+                    </p>
+                </section>
+            </div>
+
+            <footer className="footer">Copyright</footer>
         </div>
-      </aside>
-
-      <section className="content">
-        <h1>Hello world!</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eum
-          delectus ratione quaerat quis! Quasi quod, vero voluptas maxime
-          quaerat itaque odio esse modi excepturi, officiis asperiores
-          accusantium architecto ut qui sint necessitatibus est veniam magni
-          numquam adipisci. Tempore, molestias!
-        </p>
-        <h3>Bye world!</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In ullam
-          dolores odio eum ea enim hic, explicabo itaque eos fuga aliquam
-          nostrum debitis unde! Exercitationem labore sequi adipisci. Iste
-          similique molestiae ab porro facilis mollitia aliquid, nisi
-          consequuntur nam aspernatur?
-        </p>
-      </section>
-
-      <footer className="footer">Copyright</footer>
-    </div>
-  );
+    );
 }
 
 export default App;
