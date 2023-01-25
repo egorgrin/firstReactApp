3bd047d9bb117c9cd0fb5ebf00ca1d7116ef274f
egorgrin <you@example.com>
2023-01-25 23:36:03 +0500

    gradient spinning logo created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import s from './Header.module.css';
+// import logo from '../../img/logo.svg';
+import logoBg from '../../img/logo_bg.png';
+import logoFace from '../../img/logo__face.png';
 
 const Header = () => {
   return (
     <header className={s.wrapper}>
-      <div className={s.logo}>
-        <h1>Logo</h1>
+      <div className={s.logo_container}>
+        <img src={logoBg} alt="" className={s.logoBg} />
+        <img src={logoFace} alt="" className={s.logoFace} />
+      </div>
+      <div className={s.title}>
+        <h1>FIRST REACT APP</h1>
       </div>
     </header>
   );
