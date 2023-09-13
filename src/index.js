56fce53babc0e0bfb17a6395afb6369a8c6efba2
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-09-13 15:12:52 +0500

    React-Redux implementation complete. Pre "friends-page" commit

--- src/index.js
+++ src/index.js
@@ -10,28 +10,34 @@ import {Provider} from 'react-redux';
 // Create a root element for rendering the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+root.render(
+    <Provider store={store}>
+      <App state={store.getState()} dispatch={store.dispatch.bind(store)}/>
+    </Provider>,
+);
+
 // Define a function to render the app with the current state
-let renderDOM = (state) => {
+/*let renderDOM = (state) => {
   // Render the App component with the state and the addMessage function from the store
-  root.render(
+  /!*root.render(
       <Provider store={store}>
         <App state={state} dispatch={store.dispatch.bind(store)}/>
       </Provider>,
-  );
-  /*root.render(
+  );*!/
+  /!*root.render(
       <React.StrictMode>
         <App state={state} addMessage={addMessage} />
       </React.StrictMode>
-  );*/
-};
+  );*!/
+};*/
 
 // Render the app with the initial state
-renderDOM(store.getState());
+// renderDOM(store.getState());
 
 // Subscribe to the store changes and re-render the app whenever the state changes
-store.subscribe(() => {
+/*store.subscribe(() => {
   renderDOM(store.getState());
-});
+});*/
 
 // Measure performance in the app and report the results
 reportWebVitals();
