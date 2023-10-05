58a2709bbbd3d7f8dcecb5eff3e1607e8d6074d6
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-10-06 01:24:23 +0500

    Basic user authentication added (added missed files)

---
+++ src/components/UI/Textarea/Textarea.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import s from './Textarea.module.css';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPaperclip, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+
+const Textarea = ({ userInput, send, handleChange }) => {
+
+  let textareaRef = React.createRef();
+
+  return (
+      <div className={`${s.input}`}>
+        <button className={s.attachment}>
+          <FontAwesomeIcon icon={faPaperclip}/>
+        </button>
+        <textarea
+            // style={{height: height}}
+            ref={textareaRef}
+            onChange={handleChange}
+            placeholder="Type your message..."
+            value={userInput}
+        />
+        <button className={s.send} onClick={send}>
+          <FontAwesomeIcon icon={faPaperPlane}/>
+        </button>
+      </div>
+  );
+}
+
+export default Textarea;
